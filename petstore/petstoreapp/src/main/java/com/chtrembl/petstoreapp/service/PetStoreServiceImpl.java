9e37f9bbc721aaeb15c2d559bba1493d1db89d44
Dzianis Kushch <dzianis_kushch@epam.com>
2023-08-19 20:47:30 +0200

    Add application insights property

--- petstore/petstoreapp/src/main/java/com/chtrembl/petstoreapp/service/PetStoreServiceImpl.java
+++ petstore/petstoreapp/src/main/java/com/chtrembl/petstoreapp/service/PetStoreServiceImpl.java
@@ -20,7 +20,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -36,213 +35,223 @@ import java.util.stream.Collectors;
 
 @Service
 public class PetStoreServiceImpl implements PetStoreService {
-	private static final Logger logger = LoggerFactory.getLogger(PetStoreServiceImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger(PetStoreServiceImpl.class);
 
-	private final User sessionUser;
-	private final ContainerEnvironment containerEnvironment;
-	private final WebRequest webRequest;
+    private final User sessionUser;
+    private final ContainerEnvironment containerEnvironment;
+    private final WebRequest webRequest;
 
-	private WebClient petServiceWebClient = null;
-	private WebClient productServiceWebClient = null;
-	private WebClient orderServiceWebClient = null;
+    private WebClient petServiceWebClient = null;
+    private WebClient productServiceWebClient = null;
+    private WebClient orderServiceWebClient = null;
 
-	public PetStoreServiceImpl(User sessionUser, ContainerEnvironment containerEnvironment, WebRequest webRequest) {
-		this.sessionUser = sessionUser;
-		this.containerEnvironment = containerEnvironment;
-		this.webRequest = webRequest;
-	}
+    public PetStoreServiceImpl(User sessionUser, ContainerEnvironment containerEnvironment, WebRequest webRequest) {
+        this.sessionUser = sessionUser;
+        this.containerEnvironment = containerEnvironment;
+        this.webRequest = webRequest;
+    }
 
-	@PostConstruct
-	public void initialize() {
-		this.petServiceWebClient = WebClient.builder()
-				.baseUrl(this.containerEnvironment.getPetStorePetServiceURL())
-				.build();
-		this.productServiceWebClient = WebClient.builder()
-				.baseUrl(this.containerEnvironment.getPetStoreProductServiceURL()).build();
-		this.orderServiceWebClient = WebClient.builder().baseUrl(this.containerEnvironment.getPetStoreOrderServiceURL())
-				.build();
-	}
+    @PostConstruct
+    public void initialize() {
+        this.petServiceWebClient = WebClient.builder()
+                .baseUrl(this.containerEnvironment.getPetStorePetServiceURL())
+                .build();
+        this.productServiceWebClient = WebClient.builder()
+                .baseUrl(this.containerEnvironment.getPetStoreProductServiceURL()).build();
+        this.orderServiceWebClient = WebClient.builder().baseUrl(this.containerEnvironment.getPetStoreOrderServiceURL())
+                .build();
+    }
 
-	@Override
-	public Collection<Pet> getPets(String category) {
-		List<Pet> pets = new ArrayList<>();
+    @Override
+    public Collection<Pet> getPets(String category) {
+        List<Pet> pets = new ArrayList<>();
 
-		this.sessionUser.getTelemetryClient().trackEvent(
-				String.format("PetStoreApp user %s is requesting to retrieve pets from the PetStorePetService",
-						this.sessionUser.getName()),
-				this.sessionUser.getCustomEventProperties(), null);
-		try {
-			Consumer<HttpHeaders> consumer = it -> it.addAll(this.webRequest.getHeaders());
-			pets = this.petServiceWebClient.get().uri("petstorepetservice/v2/pet/findByStatus?status=available")
-					.accept(MediaType.APPLICATION_JSON)
-					.headers(consumer)
-					.header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-					.header("Cache-Control", "no-cache")
-					.retrieve()
-					.bodyToMono(new ParameterizedTypeReference<List<Pet>>() {
-					}).block();
+        this.sessionUser.getTelemetryClient().trackEvent(
+                String.format("PetStoreApp user %s is requesting to retrieve pets from the PetStorePetService",
+                        this.sessionUser.getName()),
+                this.sessionUser.getCustomEventProperties(), null);
+        try {
+            Consumer<HttpHeaders> consumer = it -> it.addAll(this.webRequest.getHeaders());
+            pets = this.petServiceWebClient.get().uri("petstorepetservice/v2/pet/findByStatus?status=available")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .headers(consumer)
+                    .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                    .header("Cache-Control", "no-cache")
+                    .retrieve()
+                    .bodyToMono(new ParameterizedTypeReference<List<Pet>>() {
+                    }).block();
 
-			// use this for look up on details page, intentionally avoiding spring cache to
-			// ensure service calls are made each for each browser session
-			// to show Telemetry with APIM requests (normally this would be cached in a real
-			// world production scenario)
-			this.sessionUser.setPets(pets);
+            // use this for look up on details page, intentionally avoiding spring cache to
+            // ensure service calls are made each for each browser session
+            // to show Telemetry with APIM requests (normally this would be cached in a real
+            // world production scenario)
+            this.sessionUser.setPets(pets);
 
-			// filter this specific request per category
-			pets = pets.stream().filter(pet -> category.equals(pet.getCategory().getName()))
-					.collect(Collectors.toList());
-			return pets;
-		} catch (WebClientException wce) {
-			this.sessionUser.getTelemetryClient().trackException(wce);
-			this.sessionUser.getTelemetryClient().trackEvent(
-					String.format("PetStoreApp %s received %s, container host: %s", this.sessionUser.getName(),
-							wce.getMessage(), this.containerEnvironment.getContainerHostName()));
-			Pet pet = new Pet();
-			pet.setName(wce.getMessage());
-			pet.setPhotoURL("");
-			pet.setCategory(new Category());
-			pet.setId((long) 0);
-			pets.add(pet);
-		} catch (IllegalArgumentException iae) {
-			// little hack to visually show the error message within our Azure Pet Store
-			// Reference Guide (Academic Tutorial)
-			Pet pet = new Pet();
-			pet.setName("petstore.service.url:${PETSTOREPETSERVICE_URL} needs to be enabled for this service to work"
-					+ iae.getMessage());
-			pet.setPhotoURL("");
-			pet.setCategory(new Category());
-			pet.setId((long) 0);
-			pets.add(pet);
-		}
-		return pets;
-	}
+            // filter this specific request per category
+            pets = pets.stream().filter(pet -> category.equals(pet.getCategory().getName()))
+                    .collect(Collectors.toList());
+            return pets;
+        } catch (WebClientException wce) {
+            this.sessionUser.getTelemetryClient().trackException(wce);
+            this.sessionUser.getTelemetryClient().trackEvent(
+                    String.format("PetStoreApp %s received %s, container host: %s", this.sessionUser.getName(),
+                            wce.getMessage(), this.containerEnvironment.getContainerHostName()));
+            Pet pet = new Pet();
+            pet.setName(wce.getMessage());
+            pet.setPhotoURL("");
+            pet.setCategory(new Category());
+            pet.setId((long) 0);
+            pets.add(pet);
+        } catch (IllegalArgumentException iae) {
+            // little hack to visually show the error message within our Azure Pet Store
+            // Reference Guide (Academic Tutorial)
+            Pet pet = new Pet();
+            pet.setName("petstore.service.url:${PETSTOREPETSERVICE_URL} needs to be enabled for this service to work"
+                    + iae.getMessage());
+            pet.setPhotoURL("");
+            pet.setCategory(new Category());
+            pet.setId((long) 0);
+            pets.add(pet);
+        }
+        return pets;
+    }
 
-	@Override
-	public Collection<Product> getProducts(String category, List<Tag> tags) {
-		List<Product> products = new ArrayList<>();
+    @Override
+    public Collection<Product> getProducts(String category, List<Tag> tags) {
+        List<Product> products = new ArrayList<>();
 
-		try {
-			Consumer<HttpHeaders> consumer = it -> it.addAll(this.webRequest.getHeaders());
-			products = this.productServiceWebClient.get()
-					.uri("petstoreproductservice/v2/product/findByStatus?status=available")
-					.accept(MediaType.APPLICATION_JSON)
-					.headers(consumer)
-					.header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-					.header("Cache-Control", "no-cache")
-					.retrieve()
-					.bodyToMono(new ParameterizedTypeReference<List<Product>>() {
-					}).block();
+        this.sessionUser.getTelemetryClient().trackEvent(
+                String.format("PetStoreApp user %s is requesting to retrieve products from the PetStoreProductService",
+                        this.sessionUser.getName()),
+                this.sessionUser.getCustomEventProperties(), null);
 
-			// use this for look up on details page, intentionally avoiding spring cache to
-			// ensure service calls are made each for each browser session
-			// to show Telemetry with APIM requests (normally this would be cached in a real
-			// world production scenario)
-			this.sessionUser.setProducts(products);
+        try {
+            Consumer<HttpHeaders> consumer = it -> it.addAll(this.webRequest.getHeaders());
+            products = this.productServiceWebClient.get()
+                    .uri("petstoreproductservice/v2/product/findByStatus?status=available")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .headers(consumer)
+                    .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                    .header("Cache-Control", "no-cache")
+                    .retrieve()
+                    .bodyToMono(new ParameterizedTypeReference<List<Product>>() {
+                    }).block();
 
-			// filter this specific request per category
-			if (tags.stream().anyMatch(t -> t.getName().equals("large"))) {
-				products = products.stream().filter(product -> category.equals(product.getCategory().getName())
-						&& product.getTags().toString().contains("large")).collect(Collectors.toList());
-			} else {
+            // use this for look up on details page, intentionally avoiding spring cache to
+            // ensure service calls are made each for each browser session
+            // to show Telemetry with APIM requests (normally this would be cached in a real
+            // world production scenario)
+            this.sessionUser.setProducts(products);
 
-				products = products.stream().filter(product -> category.equals(product.getCategory().getName())
-						&& product.getTags().toString().contains("small")).collect(Collectors.toList());
-			}
-			return products;
-		} catch (
+            // filter this specific request per category
+            if (tags.stream().anyMatch(t -> t.getName().equals("large"))) {
+                products = products.stream().filter(product -> category.equals(product.getCategory().getName())
+                        && product.getTags().toString().contains("large")).collect(Collectors.toList());
+            } else {
 
-		WebClientException wce) {
-			// little hack to visually show the error message within our Azure Pet Store
-			// Reference Guide (Academic Tutorial)
-			Product product = new Product();
-			product.setName(wce.getMessage());
-			product.setPhotoURL("");
-			product.setCategory(new Category());
-			product.setId((long) 0);
-			products.add(product);
-		} catch (IllegalArgumentException iae) {
-			// little hack to visually show the error message within our Azure Pet Store
-			// Reference Guide (Academic Tutorial)
-			Product product = new Product();
-			product.setName(
-					"petstore.service.url:${PETSTOREPRODUCTSERVICE_URL} needs to be enabled for this service to work"
-							+ iae.getMessage());
-			product.setPhotoURL("");
-			product.setCategory(new Category());
-			product.setId((long) 0);
-			products.add(product);
-		}
-		return products;
-	}
+                products = products.stream().filter(product -> category.equals(product.getCategory().getName())
+                        && product.getTags().toString().contains("small")).collect(Collectors.toList());
+            }
+            logger.info("Retrieved {} products by category {}", products.size(), category);
+            return products;
+        } catch (
 
-	@Override
-	public void updateOrder(long productId, int quantity, boolean completeOrder) {
-		this.sessionUser.getTelemetryClient()
-				.trackEvent(String.format(
-						"PetStoreApp user %s is requesting to update an order with the PetStoreOrderService",
-						this.sessionUser.getName()), this.sessionUser.getCustomEventProperties(), null);
+                WebClientException wce) {
+            this.sessionUser.getTelemetryClient().trackException(wce);
+            this.sessionUser.getTelemetryClient().trackEvent(
+                    String.format("PetStoreApp %s received %s, container host: %s", this.sessionUser.getName(),
+                            wce.getMessage(), this.containerEnvironment.getContainerHostName()));
+            // little hack to visually show the error message within our Azure Pet Store
+            // Reference Guide (Academic Tutorial)
+            Product product = new Product();
+            product.setName(wce.getMessage());
+            product.setPhotoURL("");
+            product.setCategory(new Category());
+            product.setId((long) 0);
+            products.add(product);
+        } catch (IllegalArgumentException iae) {
+            // little hack to visually show the error message within our Azure Pet Store
+            // Reference Guide (Academic Tutorial)
+            Product product = new Product();
+            product.setName(
+                    "petstore.service.url:${PETSTOREPRODUCTSERVICE_URL} needs to be enabled for this service to work"
+                            + iae.getMessage());
+            product.setPhotoURL("");
+            product.setCategory(new Category());
+            product.setId((long) 0);
+            products.add(product);
+        }
+        return products;
+    }
 
-		try {
-			Order updatedOrder = new Order();
+    @Override
+    public void updateOrder(long productId, int quantity, boolean completeOrder) {
+        this.sessionUser.getTelemetryClient()
+                .trackEvent(String.format(
+                        "PetStoreApp user %s is requesting to update an order with the PetStoreOrderService",
+                        this.sessionUser.getName()), this.sessionUser.getCustomEventProperties(), null);
 
-			updatedOrder.setId(this.sessionUser.getSessionId());
-			updatedOrder.setEmail(this.sessionUser.getEmail());
+        try {
+            Order updatedOrder = new Order();
 
-			if (completeOrder) {
-				updatedOrder.setComplete(true);
-			} else {
-				List<Product> products = new ArrayList<Product>();
-				Product product = new Product();
-				product.setId(Long.valueOf(productId));
-				product.setQuantity(quantity);
-				products.add(product);
-				updatedOrder.setProducts(products);
-			}
+            updatedOrder.setId(this.sessionUser.getSessionId());
+            updatedOrder.setEmail(this.sessionUser.getEmail());
 
-			String orderJSON = new ObjectMapper().setSerializationInclusion(Include.NON_NULL)
-					.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
-					.configure(SerializationFeature.FAIL_ON_SELF_REFERENCES, false).writeValueAsString(updatedOrder);
+            if (completeOrder) {
+                updatedOrder.setComplete(true);
+            } else {
+                List<Product> products = new ArrayList<Product>();
+                Product product = new Product();
+                product.setId(Long.valueOf(productId));
+                product.setQuantity(quantity);
+                products.add(product);
+                updatedOrder.setProducts(products);
+            }
 
-			Consumer<HttpHeaders> consumer = it -> it.addAll(this.webRequest.getHeaders());
+            String orderJSON = new ObjectMapper().setSerializationInclusion(Include.NON_NULL)
+                    .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+                    .configure(SerializationFeature.FAIL_ON_SELF_REFERENCES, false).writeValueAsString(updatedOrder);
 
-			updatedOrder = this.orderServiceWebClient.post().uri("petstoreorderservice/v2/store/order")
-					.body(BodyInserters.fromPublisher(Mono.just(orderJSON), String.class))
-					.accept(MediaType.APPLICATION_JSON)
-					.headers(consumer)
-					.header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-					.header("Cache-Control", "no-cache")
-					.retrieve()
-					.bodyToMono(Order.class).block();
+            Consumer<HttpHeaders> consumer = it -> it.addAll(this.webRequest.getHeaders());
 
-		} catch (Exception e) {
-			logger.warn(e.getMessage());
-		}
-	}
+            updatedOrder = this.orderServiceWebClient.post().uri("petstoreorderservice/v2/store/order")
+                    .body(BodyInserters.fromPublisher(Mono.just(orderJSON), String.class))
+                    .accept(MediaType.APPLICATION_JSON)
+                    .headers(consumer)
+                    .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                    .header("Cache-Control", "no-cache")
+                    .retrieve()
+                    .bodyToMono(Order.class).block();
 
-	@Override
-	public Order retrieveOrder(String orderId) {
-		this.sessionUser.getTelemetryClient()
-				.trackEvent(String.format(
-						"PetStoreApp user %s is requesting to retrieve an from with the PetStoreOrderService",
-						this.sessionUser.getName()), this.sessionUser.getCustomEventProperties(), null);
+        } catch (Exception e) {
+            logger.warn(e.getMessage());
+        }
+    }
 
-		Order order = null;
-		try {
-			Consumer<HttpHeaders> consumer = it -> it.addAll(this.webRequest.getHeaders());
+    @Override
+    public Order retrieveOrder(String orderId) {
+        this.sessionUser.getTelemetryClient()
+                .trackEvent(String.format(
+                        "PetStoreApp user %s is requesting to retrieve an from with the PetStoreOrderService",
+                        this.sessionUser.getName()), this.sessionUser.getCustomEventProperties(), null);
 
-			order = this.orderServiceWebClient.get()
-					.uri(uriBuilder -> uriBuilder.path("petstoreorderservice/v2/store/order/{orderId}").build(orderId))
-					.accept(MediaType.APPLICATION_JSON)
-					.headers(consumer)
-					.header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-					.header("Cache-Control", "no-cache")
-					.retrieve()
-					.bodyToMono(new ParameterizedTypeReference<Order>() {
-					}).block();
-		} catch (Exception e) {
-			logger.warn(e.getMessage());
-		}
-		return order;
-	}
+        Order order = null;
+        try {
+            Consumer<HttpHeaders> consumer = it -> it.addAll(this.webRequest.getHeaders());
+
+            order = this.orderServiceWebClient.get()
+                    .uri(uriBuilder -> uriBuilder.path("petstoreorderservice/v2/store/order/{orderId}").build(orderId))
+                    .accept(MediaType.APPLICATION_JSON)
+                    .headers(consumer)
+                    .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                    .header("Cache-Control", "no-cache")
+                    .retrieve()
+                    .bodyToMono(new ParameterizedTypeReference<Order>() {
+                    }).block();
+        } catch (Exception e) {
+            logger.warn(e.getMessage());
+        }
+        return order;
+    }
 
 }
